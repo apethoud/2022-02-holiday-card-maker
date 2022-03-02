b4471cb76af5bc974801f56168d032cbd5ac99c6
Andrew Pethoud <andrewpethoud@gmail.com>
2022-03-01 16:22:23 -0800

    feat(header): stub out header component

---
+++ src/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <div>
+            <div style={{ fontSize: 24, fontWeight: "bold" }}>Holiday Card Maker</div>
+        </div>
+    );
+}
+  
